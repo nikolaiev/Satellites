5812b860fc05780e3394b7b35306c97cddf087c6
nikolaiev <Vladyslav.Nikolaiev@gmail.com>
2018-10-23 23:11:29 +0300

    React point

--- app/src/components/Box/index.js
+++ app/src/components/Box/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
- export class Box extends React.Component{
+class Box extends React.Component{
     render(){
         return <div  style={{position: "absolute",
             top: "80%",
             left: "20%", borderStyle: "solid", zIndex: "1"}}>Sosdfgsdfssdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgdsfgsdfgdsfsdfggggggggggggggggggggggggggggggggggggggggsdfggsdfgsdfgme inf</div>
     }
 }
+
+export default Box;
--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './components/App/'
+import './index.css';
+import {App} from './App'
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+function reducer(state = [], action) {
+    console.log(state )
+    console.log(action)
+
+    if (action.type === 'ADD_POST') {
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import {App} from './App'
+//
+// ReactDOM.render(<App/>, document.getElementById('root'));
